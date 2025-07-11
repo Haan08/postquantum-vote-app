73d1b497c7b27d2e215d83d99c205ec860bf23fd
Haan08 <aneesunisat@gmail.com>
2025-07-11 22:35:53 +0530

    Initial commit: Post-Quantum Vote App

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function decrypt(encryptedText, keyHex) {
 // ✅ MySQL
 const db = MySQL.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
